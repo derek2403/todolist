140af92bb545d3caceebd2fbced5ae7eeee4aaf9
Liew Qi Jian <146611128+derek2403@users.noreply.github.com>
2024-12-10 01:37:35 +0800

    icon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/apple-icon-167.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Personal Dashboard" />
+        <meta name="theme-color" content="#2563eb" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
